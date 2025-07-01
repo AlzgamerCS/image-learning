21cf700f1b3636c2a6089a7f54146af608ae6375
onionmerey <bisenbinmerej@gmail.com>
2025-07-01 16:54:45 +0600

    Implemented basic model with three layers. Didn't specify the output (now it is in R). Need to normalize it

--- src/model.py
+++ src/model.py
@@ -1,9 +1,19 @@
 import torch.nn as nn
-
+import torch.nn.functional as F 
 
 class ImageMLP(nn.Module):
     def __init__(self):
-        ... # To be implemented
+        super().__init__()
+        self.fc1 = nn.Linear(2, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 1)
 
     def forward(self, x):
-        ... # To be implemented
+        x = F.relu(self.fc1(x))
+
+        x = F.relu(self.fc2(x))
+        
+        x = self.fc3(x)
+
+        return x
+
